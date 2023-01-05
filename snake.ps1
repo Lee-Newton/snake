c54f197eb9b8382cc88c129e662d33f610b482e0
Lee Newton <lgnewton@gmail.com>
2023-01-05 12:08:50 +0000

    reports of people on Mac having issue with sound. now using sound API and avoiding sendKeys. old code commented out for reference only and boilerplate in form of add-Type is required due to no automation

--- snake.ps1
+++ snake.ps1
@@ -4,6 +4,62 @@
 # |              By Lee Newton                  |
 # |---------------------------------------------|
 
+
+# The following Type is to access the Core Audio API. Original code use SendKeys but Mac users were reporting issues with this so the following boiler plate has been coppied, I have no knowledge as of yet of any support for automation hence this boilerplate code. skip along as this is only so I can access static classes like  'Volume' and 'Mute
+Add-Type -TypeDefinition @'
+using System.Runtime.InteropServices;
+[Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IAudioEndpointVolume
+{
+    // f(), g(), ... are unused COM method slots. Define these if you care
+    int f(); int g(); int h(); int i();
+    int SetMasterVolumeLevelScalar(float fLevel, System.Guid pguidEventContext);
+    int j();
+    int GetMasterVolumeLevelScalar(out float pfLevel);
+    int k(); int l(); int m(); int n();
+    int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);
+    int GetMute(out bool pbMute);
+}
+[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDevice
+{
+    int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+}
+[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDeviceEnumerator
+{
+    int f(); // Unused
+    int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+}
+[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }
+public class Audio
+{
+    static IAudioEndpointVolume Vol()
+    {
+        var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+        IMMDevice dev = null;
+        Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(/*eRender*/ 0, /*eMultimedia*/ 1, out dev));
+        IAudioEndpointVolume epv = null;
+        var epvid = typeof(IAudioEndpointVolume).GUID;
+        Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, /*CLSCTX_ALL*/ 23, 0, out epv));
+        return epv;
+    }
+    public static float Volume
+    {
+        get { float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v; }
+        set { Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, System.Guid.Empty)); }
+    }
+    public static bool Mute
+    {
+        get { bool mute; Marshal.ThrowExceptionForHR(Vol().GetMute(out mute)); return mute; }
+        set { Marshal.ThrowExceptionForHR(Vol().SetMute(value, System.Guid.Empty)); }
+    }
+}
+'@
+[Audio]::Mute = $false
+[Audio]::Volume = 0.50
+
+
 #display game title
 function DisplayTitle() {
   Clear-Host
@@ -245,7 +301,7 @@ function displayScore() {
 
 }
 
-#set system volume level
+#set system volume level - this is no longer needed as using the Audio API. keeping for refrence sake only - no calls made to this function
 function SetSystemVolume($volume) { 
   $wshShell = New-Object -com wscript.shell
   1..50 | ForEach-Object { $wshShell.SendKeys([char]174) }
@@ -293,7 +349,7 @@ DisplayInstructions
 $currcoords.x = 1
 $currcoords.y = [math]::Round($console.windowsize.height - 2)
 $console.cursorposition = $currcoords
-SetSystemVolume -Volume 50
+# SetSystemVolume -Volume 50
 $playerName = $(Write-Host "Name : " -NoNewline -ForegroundColor Cyan; Read-Host)
 
 if ($playerName -eq "exit") {
